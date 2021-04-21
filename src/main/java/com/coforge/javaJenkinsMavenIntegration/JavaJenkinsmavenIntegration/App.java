f5e4a463c9dce18479aa404b27d1d8542bbe82bb
sunnyBhadauria <sunnybhadauria29@gmail.com>
2021-04-21 15:54:42 +0530

    my Maven Jenkin Commit

---
+++ src/main/java/com/coforge/javaJenkinsMavenIntegration/JavaJenkinsmavenIntegration/App.java
@@ -0,0 +1,13 @@
+package com.coforge.javaJenkinsMavenIntegration.JavaJenkinsmavenIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
